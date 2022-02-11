20a27e5aa3f9acc2a1d97781268bded79201a4e7
Daniel <79312811+danieljbk@users.noreply.github.com>
2022-02-10 20:00:15 -0500

    Black Formatter

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def absolute_path(path):  # input relative path, output absolute path
 
 
 def personalize_script(name):
-    with open(absolute_path('script/title.txt')) as title_text:
+    with open(absolute_path("script/title.txt")) as title_text:
         fetched_title = title_text.readline().strip()
         fetched_title = fetched_title.split("{name}")
     title_text.close()
-    
-    with open(absolute_path('script/body.txt')) as body_text:
+
+    with open(absolute_path("script/body.txt")) as body_text:
         fetched_body = "".join(body_text.readlines()).strip()
         fetched_body = fetched_body.split("{name}")
     body_text.close()
-    
+
     fetched_title = fetched_title[0] + name + fetched_title[1]
     fetched_body = fetched_body[0] + name + fetched_body[1]
 
@@ -28,10 +28,16 @@ def personalize_script(name):
 
 
 def gather_personal_data():  # from relative path assets/private
-    GMAIL_USERNAME = open(absolute_path(
-        "assets/personal/gmail_username.txt"), "r").readline().strip()
-    GMAIL_API_KEY = open(absolute_path(
-        "assets/personal/gmail_app_password.txt"), "r").readline().strip()
+    GMAIL_USERNAME = (
+        open(absolute_path("assets/personal/gmail_username.txt"), "r")
+        .readline()
+        .strip()
+    )
+    GMAIL_API_KEY = (
+        open(absolute_path("assets/personal/gmail_app_password.txt"), "r")
+        .readline()
+        .strip()
+    )
 
     return GMAIL_USERNAME, GMAIL_API_KEY
 
@@ -65,21 +71,24 @@ def send():  # most important: for sending the email
         name = data[0]
         email_address = data[1]
         title, body = personalize_script(name)
-        
+
         msg = EmailMessage()
         msg.set_content(body)
-        msg['subject'] = title
-        msg['to'] = email_address
-        msg['from'] = GMAIL_USERNAME
+        msg["subject"] = title
+        msg["to"] = email_address
+        msg["from"] = GMAIL_USERNAME
 
         server.send_message(msg)
         print("    " + "- sent to:", data[1])
-    
+
     server.quit()
     print(f"\nSuccess!\n")
 
 
-if input(f"Send {len(gather_subscriber_data('subscribers.csv'))} emails? (Y/N)\n") == "Y":
+if (
+    input(f"Send {len(gather_subscriber_data('subscribers.csv'))} emails? (Y/N)\n")
+    == "Y"
+):
     send()
 else:
     print("Understood.")
